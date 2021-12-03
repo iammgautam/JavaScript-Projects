d712d2873ab2492a1cd03a59ad7f63b19bc70801
zengboi <mithileshgautam666@gmail.com>
2021-12-03 10:38:04 +0530

    digital Clock added

--- AnalogClock/script.js
+++ AnalogClock/script.js
@@ -1,6 +1,7 @@
 const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
+const digital = document.querySelector('.digi');
 
 const degreeFunc = (unit, fraction) => {
     return (unit / fraction) * 360 + 90;
@@ -27,5 +28,9 @@ function setDate() {
     const hour = now.getHours();
     const hourDegrees = degreeFunc(hour,12);;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+    digital.innerHTML = `${hour < 10 ? `0${hour}` : hour} : ${
+        mins < 10 ? `0${mins}` : mins
+      } : ${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 setInterval(setDate,1000);
